a5b0931f03554d981c9b4eb5e46cef8421ad3d2a
KLDudzinsky <kldudzinsky@gmail.com>
2020-10-21 18:36:41 -0700

    change to pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,38 +11,43 @@ import csv
 
 Bank_csv = os.path.join("PyBank", "Resources", "Bank_Resources_budget_data.csv")
 
-#create empty lists
-date = []
-profit_loss= []
-monthly_profit_change =[]
-#number of months includedd
+# Create empty lists to iterate through specific rows for the following variables
+total_months = []
+total_profit = []
+monthly_profit_change = []
+ 
 with open(Bank_csv) as csvfile: 
     csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvreader)
-    lines = len(list(csvreader))
- # sum of Profit/losses
-    total_months = sum(float(row[1]) for row in csvreader)
-#avg of change between each line 
-    for row in csvreader:
-        date.append(row[0])
-        profit_loss.append(int(row[1]))
-    #loop to get monthly changes in profits
-    for i in range(len(profit_loss)-1):
-        monthly_profit_change.append (int)
-        [profit_loss[i+1]-profit[i]] 
 
-        avg_change =round((sum(monthy_profit_change)/85),2)
-# max and min of monthly profit 
-great_increase= max(monthly_profit_change)
-great_decrease= min(monthly_profit_change)
-# Correlate max and min to month with month list and index from greatest inc and greatest dec. 
-#add + 1 at end b/c month with the change is next month
-great_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
-great_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
+    for row in csvreader: 
 
-#print
-print (f"number of months: {lines}") 
-print(f"sum of profits/lossts: {total_months}")
-print (f"Average change: $ {avg_change}" )
-print (f"Greatest Increase in Profits: {date[great_increase_month]} (${(str(great_increase))})")
-print(f"Greatest Decrease in Profits: {date[great_decrease_month]} (${(str(great_decrease))})")
+        # Append the total months and total profit to their corresponding lists
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
+        #total number of Months
+        number_months = len(total_months)
+        #total net of profits and loss
+        net_profit= sum(total_profit)
+
+    # loop through to get monthly avg change (-1 not 86 change, only 85)
+    for i in range(len(total_profit)-1):
+        
+        #append list for profit change. profit(i+1) - previous profit 
+        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+    #set avg_change
+    avg_change= round(sum(monthly_profit_change)/len(monthly_profit_change),2)
+#greatest increase and greatest decrease from monthly_profit_change list
+gr_increase_value = max(monthly_profit_change)
+gr_decrease_value = min(monthly_profit_change)
+
+# connect gr increase and gr decrease value with correct month. 
+ #use +1 at end bc month with change is actually the next month
+gr_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+gr_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
+
+print(f"Total Months: {number_months}")
+print(f"Total: $ {net_profit}")
+print(f"Average Change: $ {avg_change}")
+print(f"Greatest Increase in Profits: {total_months[gr_increase_month]} $ {gr_increase_value}")
+print(f"Greatest Decrease in Profits: {total_months[gr_decrease_month]} $ {gr_decrease_value}")
