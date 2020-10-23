dfe76072fa63c8c994e832a715bf8c0cba62a052
KLDudzinsky <kldudzinsky@gmail.com>
2020-10-22 17:13:11 -0700

    continued work on pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,18 +13,48 @@
 import os
 import csv
 
-Bank_csv = os.path.join("PyBank", "Resources", "Pypoll_Resources.csv")
+PyPoll_csv = os.path.join("PyPoll", "Resources", "Pypoll_Resources.csv")
 #names of candidates
-candidates = []
+candidate_unique=[]
 #list of number of votes for each candidate 
-num_votes = []
+candidate_num_votes = []
 #list of percentage of total votes each candidate gets 
 percent_votes = []
 
 total_votes = 0
 
-csvreader = csv.reader(csvfile, delimiter = ',')
+with open(PyPoll_csv) as csvfile: 
+    csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvreader)
-    for row in csvreader
+    for row in csvreader:
+    #add to total votes
         total_votes += 1 
-
+    #read names from row 2 of csv file
+        candidate_in = (row[2])
+    #create list of candidates who recieved votes
+    #if candidate is in list then locate the candidate by index # & add vote 
+        if candidate_in in candidate_unique:
+            candidate_index = candidate_unique.index(candidate_in)
+            candidate_num_votes[candidate_index] = candidate_num_votes[candidate_index] + 1
+        else:
+            #if candidate not in candidates_unique list then append to list & add vote
+            candidate_unique.append(candidate_in)
+            candidate_num_votes.append(1)
+            #check if working 
+            # print(f'Total votes {total_votes}')
+            # print(f'Each candidate: {candidate_unique}')
+            # print(f'Index: {candidate_unique.index(candidate_in)}')
+    #The percentage of votes each candidate won
+    perc_vote_list = []
+    votes_recieved = candidate_num_votes[0]
+    votes_index =0
+    # 'x' is loop value
+    for x in range(len(candidate_unique)):
+        vote_percentage = round(candidate_num_votes[x]/total_votes*100,2)
+        perc_vote_list.append(vote_percentage)
+        if candidate_num_votes[x]>votes_recieved:
+            votes_recieved= candidate_num_votes
+            votes_index = x
+    election_winner = candidate_unique[votes_index] 
+print (election_winner)
+print 
